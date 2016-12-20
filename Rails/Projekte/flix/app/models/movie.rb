00fd42e6041695acf9824a0bc6e18704c1522f86
hugo442 <pl-kaulich@web.de>
2016-12-20 12:57:42 +0100

    20.12.2016 Unterrichtsende

--- Rails/Projekte/flix/app/models/movie.rb
+++ Rails/Projekte/flix/app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+	has_many :reviews, dependent: :destroy
+
+	validates :name, :description, :location, presence: true
+	validates :comment, length: { minimum: 25 }
+	validates :stars, numericality: { greater_than_or_equal_to: 0 }
+
+	def free?
+		price.blank? || price.zero?
+	end
 end
--- Rails/Projekte/rails_app/config/routes.rb
+++ Rails/Projekte/rails_app/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :registrations
+	root 'events#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get "events" => "events#index"
   #get "events/:id" => "events#show"
-  resources :events
+  resources :events do
+  	resources :registrations
+  end
 end
