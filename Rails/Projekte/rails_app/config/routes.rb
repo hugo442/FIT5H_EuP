ce0165262721be5cab94b1f48bbc9984b61a953c
hugo442 <pl-kaulich@web.de>
2017-01-24 12:58:25 +0100

    Unterrichtsende 24.01.2017

--- Rails/Projekte/rails_app/config/routes.rb
+++ Rails/Projekte/rails_app/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-	root 'events#index'
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #get "events" => "events#index"
-  #get "events/:id" => "events#show"
-  resources :events do
-  	resources :registrations
-  end
+	root 'events#index'
+
+	get 'signup' => 'users#new'
+	
+	resource :session
+	resources :users
+
+  	resources :events do
+  		resources :registrations
+  	end
 end
