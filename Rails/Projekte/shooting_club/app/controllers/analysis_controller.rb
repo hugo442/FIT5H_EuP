b878adcbaf873f3e3c2b028a78ef144cf1c2d466
hugo442 <pl-kaulich@web.de>
2017-04-20 01:12:39 +0200

    Geschafft 20.04.2017

--- Rails/Projekte/shooting_club/app/controllers/analysis_controller.rb
+++ Rails/Projekte/shooting_club/app/controllers/analysis_controller.rb
@@ -13,27 +13,44 @@ class AnalysisController < ApplicationController
   	end
 
   	def calculate
-  		#@eventsall = Event.where( :name => params[:eventname] )
-
       if params[:verfahren] == "3x3 Schuss"
-        @allgroupserg = []
+        @endsummen = {}
+        @mitglieder = {}
+        @punkte = {}
+        @gruppeteiler = {}
         groups = Group.all
         groups.each do |group|
           @marksman = MarksmanGroup.where(group_id: group.id)
-          if @marksman.count >= 3
-            @wertegruppe = []
+          if @marksman.count == 4
+            @schuetzenprogruppe = []
+            @summe = 0
             @marksman.each do |marksmen|
-              if Event.where( :name => params[:eventname], :marksmen => marksmen.startnr ).count == 3
-                @wertegruppe << Event.where( :name => params[:eventname], :marksmen => marksmen.startnr )
-              elsif Event.where( :name => params[:eventname], :marksmen => marksmen.startnr ).count == 1
-                @teiler << Event.where( :name => params[:eventname], :marksmen => marksmen.startnr )
+              @wertarrayeinesschuetzen = []
+              @marksmen1 = Marksman.where(:id => marksmen.marksman_id).first
+              @zaehler = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr)
+              if @zaehler.count == 3
+                @schuetzenprogruppe << @marksmen1
+                @wertegruppe = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr)
+                @wertegruppe.each do |wert|
+                  @summe = @summe + wert.value.to_i
+                  @wertarrayeinesschuetzen << wert.value.to_i
+                end
+              elsif @zaehler.count == 1
+                @teiler = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr).first.unknown7
+                @gruppeteiler[group.id] = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr).first
+                @schuetzenprogruppe << @marksmen1
+              else
+                next
               end
+              @punkte[@marksmen1.startnr] = @wertarrayeinesschuetzen
             end
+            @mitglieder[group.id] = @schuetzenprogruppe
+            @endsummen[group.id] = [@summe, @teiler.to_f]
           else
             next
           end
         end
-
+        @endsummen = Hash[@endsummen.sort_by{|k, v| [ v[0], 1/v[1] ]}.reverse]
       elsif params[:verfahren] == "2x5 Schuss"
 
       elsif params[:verfahren] == "3 Schuss"
