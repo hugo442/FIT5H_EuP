b8ef25c001456db1d1646c45bd7b75b08be927bb
hugo442 <pl-kaulich@web.de>
2017-04-20 18:52:27 +0200

    fast geschafft

--- Rails/Projekte/shooting_club/app/controllers/analysis_controller.rb
+++ Rails/Projekte/shooting_club/app/controllers/analysis_controller.rb
@@ -17,9 +17,8 @@ class AnalysisController < ApplicationController
         @endsummen = {}
         @mitglieder = {}
         @punkte = {}
-        @gruppeteiler = {}
-        groups = Group.all
-        groups.each do |group|
+        @groups = Group.all
+        @groups.each do |group|
           @marksman = MarksmanGroup.where(group_id: group.id)
           if @marksman.count == 4
             @schuetzenprogruppe = []
@@ -37,7 +36,6 @@ class AnalysisController < ApplicationController
                 end
               elsif @zaehler.count == 1
                 @teiler = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr).first.unknown7
-                @gruppeteiler[group.id] = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr).first
                 @schuetzenprogruppe << @marksmen1
               else
                 next
@@ -52,21 +50,59 @@ class AnalysisController < ApplicationController
         end
         @endsummen = Hash[@endsummen.sort_by{|k, v| [ v[0], 1/v[1] ]}.reverse]
       elsif params[:verfahren] == "2x5 Schuss"
-
-      elsif params[:verfahren] == "3 Schuss"
-
-      elsif params[:verfahren] == "2x Teiler"
-        @eventsall = []
-        allmarksman = Marksman.all
-        allmarksman.each do |marksmen|
-          if Event.where(:name => params[:eventname], :marksmen => marksmen.startnr).count > 0
-            @eventsall << Event.where(:name => params[:eventname], :marksmen => marksmen.startnr).order(:unknown7).first
-            @eventsall << Event.where(:name => params[:eventname], :marksmen => marksmen.startnr).order(:unknown7).second
+        @endsummen = {}
+        @mitglieder = {}
+        @punkte = {}
+        @groups = Group.all
+        @groups.each do |group|
+          @marksman = MarksmanGroup.where(group_id: group.id)
+          if @marksman.count == 3
+            @schuetzenprogruppe = []
+            @summe = 0
+            @marksman.each do |marksmen|
+              @wertarrayeinesschuetzen = []
+              @marksmen1 = Marksman.where(:id => marksmen.marksman_id).first
+              @zaehler = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr)
+              if @zaehler.count == 5
+                @schuetzenprogruppe << @marksmen1
+                @wertegruppe = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr)
+                @wertegruppe.each do |wert|
+                  @summe = @summe + wert.value.to_i
+                  @wertarrayeinesschuetzen << wert.value.to_i
+                end
+              elsif @zaehler.count == 1
+                @teiler = Event.where(:name => params[:eventname], :marksmen => @marksmen1.startnr).first.unknown7
+                @schuetzenprogruppe << @marksmen1
+              else
+                next
+              end
+              @punkte[@marksmen1.startnr] = @wertarrayeinesschuetzen
+            end
+            @mitglieder[group.id] = @schuetzenprogruppe
+            @endsummen[group.id] = [@summe, @teiler.to_f]
           else
             next
           end
         end
-        @eventsall
+        @endsummen = Hash[@endsummen.sort_by{|k, v| [ v[0], 1/v[1] ]}.reverse]
+      elsif params[:verfahren] == "3 Schuss"
+
+      elsif params[:verfahren] == "2x Teiler"
+        @endsummen = {}
+        @marksman = Marksman.all
+        @marksman.each do |marksmen|
+          if Event.where(:name => params[:eventname], :marksmen => marksmen.startnr).count > 0
+            @summe = 0
+            @beideteiler = Event.where(:name => params[:eventname], :marksmen => marksmen.startnr).order(:unknown7).last(2)
+            @beideteiler.each do |event|
+              @summe = @summe + event.unknown7.to_f
+            end
+          else
+            next
+          end
+          @endsummen[marksmen.startnr] = [@summe, @beideteiler]
+        end
+        @endsummen = Hash[@endsummen.sort_by{|k, v| v[0] }]
       end
           
 
